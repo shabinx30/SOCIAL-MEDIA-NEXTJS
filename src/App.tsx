55ff25af85e2c0f90f01c26f3f1e5c783a3a6e98
shabinx30 <shabeensharih@gmail.com>
2025-01-12 21:19:45 +0530

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-  APP
-    </>
-  )
+  return <>APP</>;
 }
 
-export default App
+export default App;
--- src/Test.tsx
+++ src/Test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Test = () => {
   return (
-    <div>Test</div>
+    <div>Checking test okk</div>
   )
 }
 
