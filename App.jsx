bbe80f12665c87850085b6df9698b3106d77a991
shabinx30 <shabeensharih@gmail.com>
2025-01-13 23:24:54 +0530

    code clean up

--- App.jsx
+++
@@ -1,9 +0,0 @@
-import react from "react";
-
-import "./app.css";
-
-function App() {
-  return <div>app</div>;
-}
-
-export default App;
--- Home.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Test = () => {
-  return (
-    <div>collboration is almost ok</div>
-  )
-}
-
-export default Test
