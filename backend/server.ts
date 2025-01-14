87b093162c4bbc6a8ebf5748bb4abb53fab11246
shabinx30 <shabeensharih@gmail.com>
2025-01-14 22:42:57 +0530

    backend and frontend added

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`server running on ${process.env.PORT || 4000}`)
+})
